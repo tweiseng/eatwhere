5dcb21c2739c871a6c57488f0bdd7211e330421f
Eric Tang <t.weiseng@gmail.com>
2018-05-31 16:32:31 +0800

    random

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -3,13 +3,35 @@ require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
 let(:name) {"Sunny Restaurant"}
-let(:restaurant){Restaurant.new(name: name)}
+let(:location){"Some random address"}
+let(:restaurant){Restaurant.new(name: name, location: location)}
+# let(:resto){Restaurant.new(name:asdasf, location: "asfsdf")}
 
 	describe "#initialize" do
 	  context "with valid input" do
 	  	it "creates a new Restaurant object" do
 	  		expect(restaurant.name).to eq(name)
+	  		expect(restaurant.location).to eq(location)
+	  	end
+	  end
+
+	  context "without valid input" do
+	  	it "raises an error without valid inputs" do
+	  		expect {@resto = Restaurant.new(name:asdasf, location: "asfsdf")}.to raise_error(NameError)
 	  	end
 	  end
 	end
+
+	describe "attribute not null" do
+		context "existence of valid attribute inputs" do
+			it "creates Object with given attributes" do
+				expect(restaurant.name).to eq(name)
+	  			expect(restaurant.location).to eq(location)
+	  		end
+	  	end
+
+	  	context "lack of input for either of attributes" do
+	  		it "will not be able to create the object" do
+	  			expect
+	  end
 end
